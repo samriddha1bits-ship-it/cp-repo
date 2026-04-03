b8cc6c0944f88178793200a1714947fda4ae1e5f
samriddha1bits-ship-it <samriddha1.bits@gmail.com>
2026-04-03 20:08:49 +0530

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9, 11};
+    int target = 7;
+
+    int left = 0, right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        // Found target
+        if (arr[mid] == target) {
+            cout << "Found at index: " << mid << endl;
+            return 0;
+        }
+        // Move right
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // Move left
+        else {
+            right = mid - 1;
+        }
+    }
+
+    cout << "Not found" << endl;
+    return 0;
+}
